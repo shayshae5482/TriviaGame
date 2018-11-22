4de55d1e7fb330f0da2346037076efc998faa0b5
shayshae5482 <shayshae5482@gmail.com>
2018-11-22 11:49:14 -0600

    made the game functional + added Bootstrap

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,163 @@
-//display a time for 60 seconds
+var panel = $("#quiz-area");
 
-//show an alert that says "You have 60 seconds to answer these questions"
+// Question set
+var questions = [{
+  question: "What coverage do you need if your car is damaged in a thunderstorm?",
+  answers: ["Uninsured Motorist", "Personal Injury Protection", "Collision", "Comprehensive"],
+  correctAnswer: "Comprehensive"
+}, {
+  question: "Which of these will pay a portion of your lost wages after an accident?",
+  answers: ["Medical Payments", "PIP", "Bodily Injury", "UMBI"],
+  correctAnswer: "PIP"
+}, {
+  question: "Which insurance company's spokesperson wears an apron in most commercials?",
+  answers: ["Farmers Professor", "Progressive's Flo", "Geico's Gecko"],
+  correctAnswer: "Progressive's Flo"
+}, {
+  question: "If you are at fault in an accident, which of these will pay for the damage you caused",
+  answers: ["Comprehensive", "Uninsured Motorist Bodily Injury", "Uninsured Motorist Propert Damage", "Property Damage"],
+  correctAnswer: "Property Damage"
+}, {
+  question: "After an accident, regardless of fault, you should notify your insurance company.",
+  answers: ["Depends on the situation.", "Only if the police officer tells you to", "No", "Yes"],
+  correctAnswer: "Yes"
+}];
+
+// Variable that will hold the setInterval
+var timer;
+
+var game = {
+
+  correct: 0,
+  incorrect: 0,
+  counter: 90,
+
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
+    }
+  },
+
+  //Start the game
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>90</span> Seconds</h2>");
+
+    $("#start").remove();
+
+    for (var i = 0; i < questions.length; i++) {
+      panel.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append("<input type='radio' name='question-" + i +
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
+    }
+
+    panel.append("<button id='done'>Done</button>");
+  },
+
+  done: function() {
+
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-4']:checked"), function() {
+      if ($(this).val() === questions[4].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-5']:checked"), function() {
+      if ($(this).val() === questions[5].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-6']:checked"), function() {
+      if ($(this).val() === questions[6].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-7']:checked"), function() {
+      if ($(this).val() === questions[7].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+
+  },
+
+  result: function() {
+
+    clearInterval(timer);
+
+    $("#sub-wrapper h2").remove();
+
+    panel.html("<h2>All Done!</h2>");
+    panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
+};
+
+// CLICK EVENTS
+
+$(document).on("click", "#start", function() {
+  game.start();
+});
 
 
-//Once they press start, timer begins
-
-
-//keep track of their input
-
-
-
-//set up a function that shows the right answers
-
-function check(){
-
-var question1 = document.trivia.q1.value;
-var question2 = document.trivia.q2.value;
-var correct = 0;
-
-//if statements
-if (question1 == "Auto insurance automatically covers your medical costs after an accident.") {
-    correct++;
-}
-
-if (question2 == "Comprehensive covers your vehicle for accidents such as animal hits and hail damage.") {
-    correct++;
-}
-
-//create the else/FALSE statements to track wrong answers
-
-
-
-
-
-//make the button work once finished
-    document.getElementById("after_submit").style.visibility = "visible";
-
-    //show how many are  correct
-    document.getElementById("number_correct").innerHTML = "You answered " + correct + " correct.";
-}
-
-//the end
-        
-        
-
+$(document).on("click", "#done", function() {
+  game.done();
+});
